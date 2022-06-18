3e446d819b4138766f8fb74700e4d1ba3996e8a8
Elliot Friesen <47087071+ElliotFriesenCode@users.noreply.github.com>
2022-06-18 08:51:31 -0400

    Create update.js

---
+++ update.js
@@ -0,0 +1,10 @@
+const url = window.location.search;
+const urlParams = new URLSearchParams(url);
+const x = urlParams.get('x');
+const y = urlParams.get('y');
+const z = urlParams.get('z');
+if(x != null && y != null && z != null){
+    document.getElementById('x').innerHTML = x;
+    document.getElementById('y').innerHTML = y;
+    document.getElementById('z').innerHTML = z;
+}
